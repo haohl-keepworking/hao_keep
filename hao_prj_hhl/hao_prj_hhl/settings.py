ff6bde694ba2c938a28687464af9e8b83c4bfc7e
haohl <you@example.com>
2019-10-09 22:14:43 +0800

    1

--- hao_prj_hhl/hao_prj_hhl/settings.py
+++ hao_prj_hhl/hao_prj_hhl/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "app"
-    "swiper"
-    "user"
+    "app",
+    "user",
 ]
 
 MIDDLEWARE = [
