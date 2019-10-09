a2e6561acefe51a7321c281fa21630fb0b2f7d24
haohl <you@example.com>
2019-10-09 19:55:59 +0800

    first commit

---
+++ hao_prj_hhl/hao_prj_hhl/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hao_prj_hhl project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hao_prj_hhl.settings")
+
+application = get_wsgi_application()
